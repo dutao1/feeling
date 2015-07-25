6f568580682f4989ad1e8805fe7f2d4e3eff5032
dutao <fswh_002@163.com>
2015-07-25 09:19:21 +0800

    新增sql

--- code/feeling/feeling-baseService-core/src/main/java/com/feeling/constants/SqlConstants.java
+++ code/feeling/feeling-baseService-core/src/main/java/com/feeling/constants/SqlConstants.java
@@ -56,7 +56,7 @@ public class SqlConstants {
 	 * 取离最近的前10条记录
 	 */
 	public static final String LIST_NEAR_EVENTS="SELECT id"
-			        + " ,eid,lat,lon,location_long_code,location_hash,nick_name,mobile,create_time FROM (  SELECT id,lat,lon,location_long_code,"
+			        + " ,eid,lat,lon,location_long_code,location_hash,nick_name,mobile,event_city,create_time FROM (  SELECT id,lat,lon,location_long_code,"
 			        + " location_hash,eid,ABS(location_long_code-#{locationLongCode}) nearcode,create_time,event_city,nick_name,mobile "
 			        + " FROM event_cycle_record"
 					+"  WHERE uid<>#{uid} AND mobile <>#{mobile}   LIMIT "+NEAR_LIMIT_NUMS
